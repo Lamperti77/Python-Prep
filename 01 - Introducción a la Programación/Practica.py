70a3635c508217cf4eba368f2164014e802fb262
Claudio Lamperti <lamperticlaudio@gmail.com>
2022-05-11 20:47:38 -0300

    octavo

--- 01 - Introducción a la Programación/Practica.py
+++ 01 - Introducción a la Programación/Practica.py
@@ -1,1 +1,47 @@
+from gettext import install
+import math
+from openpyxl import NUMPY
+
+
 print('Hola Mundo!')
+numero = 20
+print(numero)
+constante = 8.5
+type(constante)
+type(numero)
+nombre = 'Claudio'
+complejo = 100 + 4j
+type (complejo)
+pi = 3.141682
+pi_redondeado = round(pi, 4)
+print(pi_redondeado)
+str = 'True'
+bool = True
+type(str)
+type(bool)
+suma = 25 + 13.14
+print(suma)
+suma_complex = (25 + 15j) + (19 + 20j)
+print(suma_complex)
+suma_mixta = 13.14 + (25 + 15j)
+print(suma_mixta)
+15*2
+2**8
+div = 27/4
+print(div)
+27//4
+27 % 4
+operandos = (6*4)+3
+print(operandos)
+nombre_completo = 'Claudio' + 'Lamperti'
+print(nombre_completo)
+dos = '2'
+dos == 2
+dos = int(dos)
+dos == 2
+a = float('3,8')
+tres = 3
+tres -= tres
+print(tres)
+1<<2
+2+'2'
